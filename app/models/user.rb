dbe6e0e0be3d5aac17237573ddc5c86c0bd352a9
Austin Kilgore <mr.kilgore@MacBook-Air.local>
2022-04-09 17:44:00 -0600

    push

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :campaigns, dependent: :destroy
+  has_many :donations, dependent: :destroy
 end
