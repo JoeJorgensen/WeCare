411e56121fdd78da439a07dc59d0a7f0efade192
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-04-11 14:49:07 -0700

    campaign controller/ route

---
+++ app/controllers/api/campaigns_controller.rb
@@ -0,0 +1,49 @@
+class Api::CampaignsController < ApplicationController
+
+  #defines the CRUD actions before using them
+  before_action :set_campaign, only: [:show, :destroy, :update]
+
+   #finds all Campaigns in DB
+  def index
+  render json: Campaign.all
+  end
+
+  #finds specific Campaign in DB
+  def show 
+  render json: @campaign
+  end
+
+  #creates a Campaign in DB
+  def create 
+    campaign = Campaign.new(campaign_params)
+    if(campaign.save)
+      render json: campaign
+    else
+      render json: {errors: campaign.errors.full_messages}, status: 422
+    end
+end
+
+#updates a Campaign in DB (edit)
+def update 
+  if(@campaign.update(campaign_params))
+    render json: @campaign
+  else
+    render json: {errors: @campaign.errors.full_messages}, status: 422
+ end
+end
+
+#deletes a Campaign in DB
+def destroy 
+  render json: @campaign.destroy
+end
+
+private
+#function that allows @campaign to find specific campign
+def set_campaign
+    @campaign = Campaign.find(params[:id])
+end
+
+def campaign_params
+  params.require(:campaign).permit(:name, :description, :image, :goal, :expiration, :user_id)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :campaigns
+  end
 end
