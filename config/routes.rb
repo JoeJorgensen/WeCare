2316f13c5bb50c2f598de1d09fbcbb94bcbcba4b
Austin Kilgore <mr.kilgore@MacBook-Air.local>
2022-04-12 14:39:18 -0600

    pulling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do
-<<<<<<< HEAD
-    resources :categories
-=======
     resources :categories 
->>>>>>> a73b81cf364d5ff24e3604d76de94acd6d3d46ba
     resources :campaign_categories
     resources :users
     resources :campaigns do
@@ -14,9 +10,5 @@ Rails.application.routes.draw do
       end
       get '/donations', to: 'donations#index_of_all'
   end
-<<<<<<< HEAD
-end
-=======
 
 end
->>>>>>> a73b81cf364d5ff24e3604d76de94acd6d3d46ba
