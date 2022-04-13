a78a7ee0dc7c065d9e7802f4b4b694185201f8ad
JoeJorgensen <joebjorg@gmail.com>
2022-04-13 12:37:15 -0600

    profile page working goood

--- client/src/components/auth/MyProfile.js
+++ client/src/components/auth/MyProfile.js
@@ -7,20 +7,31 @@ import "filepond/dist/filepond.min.css";
 
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+// import FilePondPluginImageCrop from "filepond-plugin-image-crop";
+// import FilePondPluginImageTransform from 'filepond-plugin-image-transform'
+import 'filepond/dist/filepond.min.css';
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 import axios from "axios";
 
 import Button from "react-bootstrap/esm/Button";
 import Badge from "react-bootstrap/esm/Badge";
+import Alert from "react-bootstrap/esm/Alert";
+
 import Card from "../../providers/Card";
 
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+registerPlugin(
+  FilePondPluginImageExifOrientation, 
+  FilePondPluginImagePreview,
+  // FilePondPluginImageCrop,
+  // FilePondPluginImageTransform,
+  );
+
 
 function MyProfile() {
-  const [files, setFiles] = useState([]);
-  const [name, setName] = useState("");
-
   const { user, setUser } = useContext(AuthContext);
+  const [files, setFiles] = useState(null);
+  const [name, setName] = useState(user.name);
+
 
   const handleUpdate = (files) => {
     setFiles(files);
@@ -34,6 +45,7 @@ function MyProfile() {
     try {
       console.log("trying to update with data:");
       let res = await axios.put("/api/update_image", data);
+      console.log('after await handleImage')
       setUser(res.data);
     } catch (err) {
       alert("error occurred updating");
@@ -46,9 +58,23 @@ function MyProfile() {
     try{
       let res = await axios.put(`/api/users/${user.id}`, {name})
       setUser(res.data)
-    }catch(err){
+      if(files){
+        await handleImage() }
+      console.log('after await handleImage in handleSubmit')
+    }catch(err){ 
       alert('error updating user info')
-    }finally{handleImage()}
+    }finally{
+      // document.location.reload()
+      window.scrollTo(0,0)
+
+    }
+
+    
+      
+   
+    
+    
+     
   }
 
   return (
@@ -61,18 +87,29 @@ function MyProfile() {
       </Badge>
       <br />
       <br />
-      <Badge>{user.name}</Badge>
+      <Badge><h5>{user.name}</h5></Badge>
        {!user.name && <p>no name</p>}
+      <br/>
+      <br/>
+      <Badge><h5>Balance: ${user.balance}</h5></Badge>
+       {!user.balance && <p>You broke</p>}
       <br/>
       <br/>
 
      
 
 
-      {user.image && <img src={user.image} width={300} />}
+      {user.image && <img 
+      style={{
+      borderRadius: '50%', 
+      width: '200px', 
+      height: '200px',
+      aspectRatio: 'auto'
+    }}
+       src={user.image} width={300}  />}
       {!user.image && <p>no image</p>}
       <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Badge bg="dark">
             <h1>Update Profile</h1>
         
@@ -85,7 +122,9 @@ function MyProfile() {
           <br />
           <br />
 
-          <input value={name} onChange={(e) => setName(e.target.value)} />
+          <input value = {name} required placeholder= {user.name} onChange={(e) => setName(e.target.value)} />
+ 
+          
           <br />
           <br />
 
@@ -96,12 +135,15 @@ function MyProfile() {
           <br />
 
           <FilePond
+          allowImageCrop={true}
+          allowImageTransform={true}
+          imageCropAspectRatio={'1:1'}
             files={files}
             allowMultiple={false}
             onupdatefiles={handleUpdate}
             labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
           />
-          <Button onClick={handleSubmit}>Update Profile</Button>
+          <Button type ='submit'>Update Profile</Button>
         </form>
       </Card>
     </div>
