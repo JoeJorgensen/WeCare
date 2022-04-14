13ff693599bb0da72dc83c70f54f3a34a45e68ac
Austin Kilgore <mr.kilgore@MacBook-Air.local>
2022-04-14 12:21:30 -0600

    initial commit

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,9 @@
-class StaticController < ApplicationController
+require "rails/application_controller"
+
+class StaticController < Rails::ApplicationController
+  layout false
+
   def index
+    render file: Rails.root.join("public", "index.html")
   end
 end
