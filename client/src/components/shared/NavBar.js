f5bfbb881dae099ed18405bc43cdac2100319712
Austin Kilgore <mr.kilgore@MacBook-Air.local>
2022-04-21 12:32:35 -0600

    env

--- client/src/components/shared/NavBar.js
+++ client/src/components/shared/NavBar.js
@@ -17,19 +17,17 @@ const NavBar = () => {
     if (auth.user) {
       return (
         <div style={{ alignItems: "center" }} className="navbar-nav">
+              {/* <Nav.Link href="/my_donations">My Donations</Nav.Link> */}
+
           <Nav.Link href="/my_profile">
             {user.image && (
-              <img
-                style={{
+              <img style={{
                   objectFit: "cover",
                   borderRadius: "50%",
                   width: "40px",
                   height: "40px",
                 }}
-                src={user.image}
-                width={300}
-              />
-            )}
+                src={user.image} width={300}/>)}
           </Nav.Link>
           
         </div>
@@ -79,23 +77,8 @@ const NavBar = () => {
               {/* <Nav.Link href="/home">Home</Nav.Link> */}
               <Nav.Link href="/feed">Home</Nav.Link>
               <Nav.Link href="/about">About</Nav.Link>
-
-              <NavDropdown
-                bg="dark"
-                variant="dark"
-                title="Fundraising"
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="/categories">
-                  Categories
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/new_campaign">
-                  New Campaign
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/my_donations">
-                  My Donations
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="/new_campaign">New Campaign</Nav.Link>
+              <Nav.Link href="/my_donations">My Donations</Nav.Link>
               {renderLeftNav()}
             </Nav>
             {renderRightNav()}
