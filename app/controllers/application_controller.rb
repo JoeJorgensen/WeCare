4690bd51d852da703037d0f1e1edb544489ec457
Austin Kilgore <mr.kilgore@MacBook-Air.local>
2022-04-27 16:25:50 -0600

    save username fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        before_action :configure_permitted_parameters, if: :devise_controller?
+
+        protected
+
+        def configure_permitted_parameters
+                updated_attrib = []
+                added_attrib = [:name]
+                devise_parameter_sanitizer.permit :sign_up, keys: added_attrib
+                devise_parameter_sanitizer.permit :account_update, keys: updated_attrib
+        end
 end
