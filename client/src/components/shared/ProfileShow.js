a161c4721a530f5b26313792e71a2e3fedc4604a
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-04-27 15:46:00 -0700

    sql render updates in users controller

--- client/src/components/shared/ProfileShow.js
+++ client/src/components/shared/ProfileShow.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ProfileShow = () => {
-  const [profiles, setProfiles] = useState([]);
+  const [donations, setDonations] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [campaigns, setCampaigns] = useState([]);
 
   const params = useParams();
 
@@ -15,7 +17,12 @@ const ProfileShow = () => {
     try {
       let res = await axios.get(`/api/users_campaigns_donated_to/${params.id}`);
       console.log(res.data);
-      setProfiles(res.data);
+      //Profile clicked campaigns that they donated to
+      setDonations(res.data.donations);
+      //Profile clicked info (bio, img, ect)
+      setUsers(res.data.user);
+      //Profile clicked campaigns they created
+      setCampaigns(res.data.campaigns);
     } catch (error) {
       console.log(error);
     }
@@ -23,18 +30,38 @@ const ProfileShow = () => {
 
   //User campaigns that they donated to rendering
   const userProfileData = () => {
-    return profiles.map((p) => {
+    return donations.map((d) => {
       return (
-        <div key={p.id}>
-          {p.name}
-          {p.user_name}
-          {p.amount}
-          {p.campaign_id}
-          {p.user_id}
-          {p.image}
-          {p.campaign_image}
-          {p.comment}
-          {p.created_at}
+        <div key={d.id}>
+          {d.name}
+          {d.user_name}
+          {d.amount}
+          {d.campaign_id}
+          {d.user_id}
+          {d.image}
+          {d.campaign_image}
+          {d.comment}
+          {d.created_at}
+        </div>
+      );
+    });
+  };
+
+  const userInfoData = () => {
+    return users.map((u) => {
+      return (
+        <div key={u.id}>
+          {u.bio} {u.image} {u.name} {u.id}
+        </div>
+      );
+    });
+  };
+
+  const userCampaignData = () => {
+    return campaigns.map((c) => {
+      return (
+        <div key={c.id}>
+          {c.name} {c.description} {c.image} {c.current_amount} {c.id}
         </div>
       );
     });
@@ -44,8 +71,12 @@ const ProfileShow = () => {
     <>
       <div style={{ height: "73vh" }}>
         <h1>Profile Show</h1>
-        <p>{JSON.stringify(profiles)}</p>
-        <p>{userProfileData()}</p>
+        <p>{JSON.stringify(donations)}</p>
+        <p>{JSON.stringify(users)}</p>
+        <p>{JSON.stringify(campaigns)}</p>
+        <p>Campaigns they donated to data:{userProfileData()}</p>
+        <p>Info about Users profile data:{userInfoData()}</p>
+        <p>Campaigns the profile clicked has created:{userCampaignData()}</p>
       </div>
     </>
   );
