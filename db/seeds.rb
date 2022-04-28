05a13e5a4e256609fcca6ce6f6279c1a77ab0634
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-04-28 14:54:20 -0700

    pull

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,13 +17,14 @@ u7 = User.create(name: 'Randy', email: 'randy@test.com', password: '123456', ima
 
 
 
-c1 = Campaign.create(name: 'Matt Hays Memorial Fund', description: 'Matt Hays Memorial', image: 'https://2dbdd5116ffa30a49aa8-c03f075f8191fb4e60e74b907071aee8.ssl.cf1.rackcdn.com/6879821_1447383818.6328.jpg', current_amount: 5000.00, goal: 15000.00, expiration: 'May 3rd 2023 10:10', user_id:u1.id)
+c1 = Campaign.create(name: 'Matt Hays Memorial Fund', description: 'Matt Hays Memorial', image: 'https://2dbdd5116ffa30a49aa8-c03f075f8191fb4e60e74b907071aee8.ssl.cf1.rackcdn.com/6879821_1447383818.6328.jpg', current_amount: 5000.00, goal: 15000.00, expiration: 'May 3rd 2023 10:10', user_id:u1.id, category_id:cat1.id)
 c2 = Campaign.create(name: 'Barn Fire', description: 'Barn has been burnt up', image: 'https://img.hobbyfarms.com/wp-content/uploads/2018/01/18040448/barn-fire-safety-1928639.jpg', current_amount: 2000.00, goal: 20000.00, expiration: 'June 16th 2023 10:10', user_id:u2.id)
 c3 = Campaign.create(name: 'House Fire', description: 'House has been burnt up', image: 'https://www.puroclean.com/wp-content/uploads/2019/08/AdobeStock_166647716-1.jpeg', current_amount: 4500.00, goal: 25000.00, expiration: 'April 1st 2013 10:10', user_id:u2.id)
 c4 = Campaign.create(name: 'New Business', description: 'Opening up a new Business in NYC', image: 'https://www.laytoncity.org/LC/CommunityCalendar/RenderImageOrFile/0abce416-ee2d-4da6-97ce-4f6d4e554867', current_amount: 1750.00, goal: 15000.00, expiration: 'December 1st 2023 10:10', user_id:u5.id)
 c5 = Campaign.create(name: 'Forge Fundraising', description: 'New Blacksmith in town', image: 'https://nt.global.ssl.fastly.net/images/1431820118708-webericmcdonaldanvilfireattheforgeeastdevonbranscombe.jpg?width=1920&auto=webp&crop=16:7', current_amount: 9500.00, goal: 25000.00, expiration: 'November 1st 2023 10:10', user_id:u4.id)
 c6 = Campaign.create(name: 'Vegas Kids Basketball Program', description: 'AAU team for the community', image: 'https://vegasfamilyevents.com/wp-content/uploads/2019/09/vegas-kids-sports-featured-image-640x480.jpg', current_amount: 4250.00, goal: 22500.00, expiration: 'September 10th 2023 10:10', user_id:u7.id)
 c7 = Campaign.create(name: 'Utah Kids Football Program', description: 'Youth kids, Football 4 Families', image: 'https://az388273.vo.msecnd.net/campsystem/images/1/CampAccountBanner_50e7aa13a8854af69583f3b79048af8b.jpg', current_amount: 7000.00, goal: 26000.00, expiration: 'October 26th 2023 10:10', user_id:u6.id)
+c8 = Campaign.create(name: 'Michaels Steam', description: 'For the best TA ever', image: 'https://png2.cleanpng.com/sh/8431bff8ab91e3bf7ecc23a80a7ec68d/L0KzQYm3VcEyN6ZnfZH0aYP2gLBuTfNwdaF6jNd7LXnmf7B6TgN1bZJyRadqZkXkR4G8UcM0P2Y6RqUDNES0RYS6UcUzPmE5UKc6N0C6SIi1kP5o/kisspng-computer-icons-steam-5af5a705133755.3844153315260485170787.png', current_amount: 1.00, goal: 110.00, expiration: 'September 10th 2023 10:10', user_id:u7.id)
 
 d1 = Donation.create(comment: 'Hope everything is good', amount: 100.00, anonymous: false, user_id:u1.id, campaign_id:c1.id, created_at: 'May 5th 2020 09:10')
 d2 = Donation.create(comment: 'Oh no, the barn', amount: 300.00, anonymous: false, user_id:u2.id, campaign_id:c2.id, created_at: 'June 16th 2021 10:10')
@@ -58,6 +59,7 @@ cat3 = Category.create(name:'Sports')
 cat4 = Category.create(name:'Business')
 cat5 = Category.create(name:'Hobbies')
 cat6 = Category.create(name:'Events')
+
 
 
 camCat1 = CampaignCategory.create(category_id:cat1.id, campaign_id:c1.id)
