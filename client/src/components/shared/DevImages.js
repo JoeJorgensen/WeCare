eeacdde852b918ea641445cf63f080a0b3e837f9
Austin Kilgore <mr.kilgore@MacBook-Air.local>
2022-05-02 12:05:21 -0600

    about page update urls needed

--- client/src/components/shared/DevImages.js
+++ client/src/components/shared/DevImages.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
 import behance from "./Images/BlueBehance.png"
+import insta from "./Images/instagram.png"
+import github from "./Images/githubimg.png"
+import twitter from "./Images/BlueTwitter.png"
+import facebook from "./Images/facebook.png"
+import linkedin from "./Images/Linkedin.png"
+import twitch from "./Images/twitch.png"
+import youtube from "./Images/youtube.png"
 
 
 function DevImages(props) {
@@ -8,49 +15,89 @@ function DevImages(props) {
 <div className="imagescontainer">
   <div className="card card0">
     <div className="border1">
-      <h2>Joe Jorgensen</h2>
+      <h3>Joe Jorgensen</h3>
       <div className="icons">
-        <a className="fa fa-codepen" image-url="https://logodix.com/logo/14591.jpg" aria-hidden="false" href="" ></a>
-        <i className="fa fa-instagram" image={behance} aria-hidden="false" href="" ></i>
-        <i className="fa fa-dribbble" src="https://logodix.com/logo/14591.jpg" aria-hidden="false" href="" ></i>
-        <i className="fa fa-twitter" image="https://logodix.com/logo/14591.jpg" aria-hidden="false" href="" ></i>
-        <i className="fa fa-facebook" url="https://logodix.com/logo/14591.jpg" aria-hidden="false" href="" ></i>
+      <a href="">
+              <img src={insta} className="fa fa-instagram" width='20px'/>
+            </a>
+            <a href="">
+              <img src={github} className="fa fa-github" width='20px'/>
+            </a>
+            <a href="">
+              <img src={twitter} className="fa fa-twitter" width='20px'/>
+            </a>
+            <a href="">
+              <img src={facebook} className="fa fa-facebook" width='20px'/>
+            </a>
+            <a href="">
+              <img src={linkedin} className="fa fa-linkedin" width='20px'/>
+            </a>
       </div>
     </div>
   </div>
   <div className="card card1">
     <div className="border1">
-      <h2>Steven Crass</h2>
+      <h3>Steven Crass</h3>
       <div className="icons">
-        <i className="fa fa-codepen" aria-hidden="true"></i>
-        <i className="fa fa-instagram" aria-hidden="true"></i>
-        <i className="fa fa-dribbble" aria-hidden="true"></i>
-        <i className="fa fa-twitter" aria-hidden="true"></i>
-        <i className="fa fa-facebook" aria-hidden="true"></i>
+      <a href="">
+              <img src={insta} className="fa fa-instagram" width='20px'/>
+            </a>
+            <a href="">
+              <img src={github} className="fa fa-github" width='20px'/>
+            </a>
+            <a href="">
+              <img src={twitter} className="fa fa-twitter" width='20px'/>
+            </a>
+            <a href="">
+              <img src={facebook} className="fa fa-facebook" width='20px'/>
+            </a>
+            <a href="">
+              <img src={linkedin} className="fa fa-linkedin" width='20px'/>
+            </a>
       </div>
     </div>
   </div>
   <div className="card card2">
     <div className="border1">
-      <h2>Randy Clements</h2>
+      <h3>Randy Clements</h3>
       <div className="icons">
-        <i className="fa fa-codepen" aria-hidden="true"></i>
-        <i className="fa fa-instagram" aria-hidden="true"></i>
-        <i className="fa fa-dribbble" aria-hidden="true"></i>
-        <i className="fa fa-twitter" aria-hidden="true"></i>
-        <i className="fa fa-facebook" aria-hidden="true"></i>
+      <a href="">
+              <img src={insta} className="fa fa-instagram" width='20px'/>
+            </a>
+            <a href="">
+              <img src={github} className="fa fa-github" width='20px'/>
+            </a>
+            <a href="">
+              <img src={twitter} className="fa fa-twitter" width='20px'/>
+            </a>
+            <a href="">
+              <img src={facebook} className="fa fa-facebook" width='20px'/>
+            </a>
+            <a href="">
+              <img src={linkedin} className="fa fa-linkedin" width='20px'/>
+            </a>
       </div>
     </div>
   </div>
   <div className="card card3">
     <div className="border1">
-      <h2>Austin Kilgore</h2>
+      <h3>Austin Kilgore</h3>
       <div className="icons">
-        <i className="fa fa-codepen" aria-hidden="true"></i>
-        <i className="fa fa-instagram" aria-hidden="true"></i>
-        <i className="fa fa-dribbble" aria-hidden="true"></i>
-        <i className="fa fa-twitter" aria-hidden="true"></i>
-        <i className="fa fa-facebook" aria-hidden="true"></i>
+      <a href="https://www.instagram.com/mr_kilgore_gaming/">
+              <img src={insta} className="fa fa-instagram" width='20px'/>
+            </a>
+            <a href="https://github.com/MrKilgore94">
+              <img src={github} className="fa fa-github" width='20px'/>
+            </a>
+            <a href="https://www.linkedin.com/in/austinskilgore/">
+              <img src={linkedin} className="fa fa-linkedin" width='20px'/>
+            </a>
+            <a href="https://www.twitch.tv/mr_kilgore_gaming">
+              <img src={twitch} className="fa fa-twitch" width='20px'/>
+            </a>
+            <a href="https://www.youtube.com/channel/UCqr--mhPzgywG7FwIadN2zQ">
+              <img src={youtube} className="fa fa-youtube" width='20px'/>
+            </a>
       </div>
     </div>
   </div>
